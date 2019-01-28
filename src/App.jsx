4aef1d135fd277aa862fff6c3a66a57d214d7dd7
Lucas Mallmann <lucas.ribeiro@object1ve.com>
2019-01-28 13:03:59 -0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Layout } from './components/UI/Layout/Layout';
+import { HomeContainer } from './containers/HomeContainer/HomeContainer';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Ola
+      </div>
+    );
+  }
+}
+
+export default App;
